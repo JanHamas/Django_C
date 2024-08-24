f838ecb395f6bef180a3398e739df9346b439dde
JanHamas <hamasjan82@gmail.com>
2024-08-24 03:11:40 -0700

    adslfkj

--- project1/home/views.py
+++ project1/home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+import datetime
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    data = {
+        "stu1":{"name":"hamas"}
+    }
+        
+    return render(request,'index.html',student)
