25dbdf244fe2608b53f197dbbea465f01b4af11e
JanHamas <hamasjan82@gmail.com>
2024-08-23 21:41:30 -0700

    kk

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = BASE_DIR / 'templates',
-STATIC_DIR = BASE_DIR / 'static/',
+TEMPLATE_DIR = BASE_DIR / 'templates'
+STATIC_DIR = BASE_DIR / 'static/'
 STATICFILES_DIR = STATIC_DIR,
 
 # Quick-start development settings - unsuitable for production
